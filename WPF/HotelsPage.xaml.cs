f5a6962218e918a916d718263ac73701d3da7f3f
KostinMakar <90038776+KostinMakar@users.noreply.github.com>
2021-11-17 13:01:24 +0300

    Update HotelsPage.xaml.cs

--- WPF/HotelsPage.xaml.cs
+++ WPF/HotelsPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,11 +23,27 @@ namespace ToursApp
         public HotelsPage()
         {
             InitializeComponent();
+            DGridHotels.ItemsSource = ToursKMEntities.GetContext().Hotel.ToList();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
