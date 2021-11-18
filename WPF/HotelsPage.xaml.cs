92fca1a17586a702ec2175459366c42516281a67
KostinMakar <90038776+KostinMakar@users.noreply.github.com>
2021-11-18 09:54:55 +0300

    Update HotelsPage.xaml.cs

--- WPF/HotelsPage.xaml.cs
+++ WPF/HotelsPage.xaml.cs
@@ -20,30 +20,55 @@ namespace ToursApp
     /// </summary>
     public partial class HotelsPage : Page
     {
+
         public HotelsPage()
         {
             InitializeComponent();
-            DGridHotels.ItemsSource = ToursKMEntities.GetContext().Hotel.ToList();
+            //DGridHotels.ItemsSource = ToursKMEntities.GetContext().Hotel.ToList();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            
         }
 
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
-
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
         }
 
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+            Manager.MainFrame.Navigate(new AddEditPage(null));
         }
 
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
         {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
 
+           if (MessageBox.Show($"Вы точно хотите удалить следующие {hotelsForRemoving.Count()} элементов?", "Внимание",
+               MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    ToursKMEntities.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    ToursKMEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                ToursKMEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = ToursKMEntities.GetContext().Hotel.ToList();
+            }
         }
     }
 }
